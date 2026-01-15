0376a8541ccd4855b2786867d5a8b6da7f1dd057
karub360 <karub360@gmail.com>
2026-01-15 16:11:21 +0300

    Temel analiz fonksiyonu eklendi

--- ai-backend/main.py
+++ ai-backend/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from textblob import TextBlob
 
 app = FastAPI()
+
+class VeriModeli(BaseModel):
+    metin: str
 
 @app.get("/")
 def read_root():
     return {"Durum": "Calisiyor", "Servis": "AI Backend"}
+
+@app.post("/analiz")
+def analiz_yap(veri: VeriModeli):
+    analiz = TextBlob(veri.metin)
+    skor = analiz.sentiment.polarity
+    
+    karar = "Notr"
+    if skor > 0.05:
+        karar = "Olumlu"
+    elif skor < -0.05:
+        karar = "Olumsuz"
+
+    return {"skor": skor, "karar": karar}
