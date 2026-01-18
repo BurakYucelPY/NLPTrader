39597a27fbadcb237b0722e54f5b2bf1cfa603b7
karub360 <karub360@gmail.com>
2026-01-19 02:00:03 +0300

    Rota yapılandırması güncellendi

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,13 +2,13 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { ROUTES } from './paths.jsx'
 import HomePage from '../pages/HomePage'
-import AnalysisPage from '../pages/AnalysisPage'
+import BTCPage from '../pages/BTCPage'
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path={ROUTES.HOME} element={<HomePage />} />
-      <Route path={ROUTES.ANALYSIS} element={<AnalysisPage />} />
+      <Route path={ROUTES.ANALYSIS} element={<BTCPage />} />
       <Route path="*" element={<Navigate to={ROUTES.HOME} replace />} />
     </Routes>
   )
